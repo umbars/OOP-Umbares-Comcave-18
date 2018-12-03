e5cc4a4d79df2aadde5bd6c7e58a9c3de897bee1
umbares <dest1biz@gmail.com>
2018-12-03 13:07:29 +0100

    12

--- Getter Setter/src/Daten.java
+++ Getter Setter/src/Daten.java
@@ -1,16 +1,22 @@
 
 public class Daten {
 
-	private int zahl1=5;
+	private int zahl1 = 5;
 	private String name = "Thomas";
-	public int getZahl1(){
-	return this.zahl1;
+	
+	public int getZahl1() {
+		return this.zahl1;
+	}
+	
 	public String getName() {
+		return this.name;
+	}
+	
 	
 	public void setZahl1(int zahl1) {
 		this.zahl1=zahl1;
 	}
-	public void setName() {
+	public void setName(String name) {
 		this.name = name; 
 	}
 }
